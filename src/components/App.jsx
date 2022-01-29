f6c4e81783176b8f6a3d89dd08b434c0228e5242
Ali Shah <s.ashah@outlook.com>
2022-01-29 08:58:47 -0500

    [Init]

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Salaam, world 😊
+      </h1>
+    </div>
+  );
+}
+
+export default App;
