931fb82c6fe1954fd9d68ff0be021da8ba66d710
Ali Shah <s.ashah@outlook.com>
2022-02-08 04:54:45 -0500

    [Added] redux boilerplate

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const ACTION = 'ACTION';
+
+export const actionFunction = (payload) => ({
+  type: ACTION,
+  payload: { payload },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import rootReducer from './reducers';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable no-underscore-dangle */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
